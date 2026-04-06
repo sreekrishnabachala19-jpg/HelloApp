5fe02e11377a58d85db3e91502a6a966174d0ba0
sreekrishnabachala19-jpg <sreekrishnabachala19@gmail.com>
2026-04-06 14:45:33 +0530

    UC6.java

---
+++ UC6.java
@@ -0,0 +1,19 @@
+class Greet {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
